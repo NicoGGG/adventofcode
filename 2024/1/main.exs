d022e6410ec424008ae8f8895c4f3c10d3753482
NicoGGG <coolranger@protonmail.com>
2024-12-02 22:54:31 +0100

    2024: day 1 part 2

--- 2024/1/main.exs
+++ 2024/1/main.exs
@@ -1,15 +1,28 @@
-result =
+two_columns =
   File.stream!("file.txt")
   |> Enum.map(&String.trim/1)
   |> Enum.map(&String.split/1)
   |> Enum.map(fn inner_list -> Enum.map(inner_list, &String.to_integer/1) end)
   |> Enum.map(&List.to_tuple/1)
   |> Enum.unzip()
-  |> Tuple.to_list()
+
+result_part_1 =
+  Tuple.to_list(two_columns)
   |> Enum.map(fn inner_list -> Enum.sort(inner_list) end)
   |> Enum.zip()
   |> Enum.map(&Tuple.to_list/1)
   |> Enum.map(fn inner_list -> Enum.reduce(inner_list, fn x, acc -> abs(acc - x) end) end)
   |> Enum.reduce(fn x, acc -> acc + x end)
 
-IO.inspect(result)
+IO.inspect(result_part_1)
+
+list_1 = elem(two_columns, 0)
+list_2 = elem(two_columns, 1)
+
+result_part_2 =
+  Enum.map(list_1, fn element ->
+    element * Enum.count(Enum.filter(list_2, fn x -> x == element end))
+  end)
+  |> Enum.sum()
+
+IO.inspect(result_part_2)
